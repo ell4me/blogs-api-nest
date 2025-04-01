75e9a356d496365c9b05e78176e7586c3bbc8e7c
Danil Sennikov <d.sennikov@banki.ru>
2025-04-01 20:19:25 +0300

    fix modules

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -16,6 +16,10 @@ import { CommonConfig } from '../../common/config/common.config';
 import { LikesPostModule } from '../likes-post/likes-post.module';
 import { SecurityDevicesModule } from '../security-devices/security-devices.module';
 import { LikesCommentModule } from '../likes-comment/likes-comment.module';
+import { QuizQuestionsModule } from '../quiz-game/quiz-questions/quiz-questions.module';
+import { PairsQuizQuestionModule } from '../quiz-game/pairs-quiz-question/pairs-quiz-question.module';
+import { PairsQuizAnswerModule } from '../quiz-game/pairs-quiz-answer/pairs-quiz-answer.module';
+import { PairsQuizModule } from '../quiz-game/pairs-quiz/pairs-quiz.module';
 
 @Module({
   imports: [
@@ -79,6 +83,10 @@ import { LikesCommentModule } from '../likes-comment/likes-comment.module';
     AuthModule,
     SecurityDevicesModule,
     LikesCommentModule,
+    PairsQuizModule,
+    QuizQuestionsModule,
+    PairsQuizQuestionModule,
+    PairsQuizAnswerModule,
   ],
 })
 export class AppModule {
