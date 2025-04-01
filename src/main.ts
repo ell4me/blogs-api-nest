b9c47d8098d27c9ff9a7636a2b394b78d7f36395
Danil Sennikov <d.sennikov@banki.ru>
2025-04-01 21:53:46 +0300

    Revert "test"
    This reverts commit 324622ec6dbf9c8e556613737d951a55dd8a98e4.

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,15 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './modules/app/app.module';
 import { useAppSettings } from './common/helpers/useAppSettings';
+import { CommonConfig } from './common/config/common.config';
 
 async function bootstrap() {
   // из-за того, что нам нужно донастроить динамический AppModule, мы не можем сразу создавать приложение,
   // а создаём сначала контекст
   const appContext = await NestFactory.createApplicationContext(AppModule);
-  // const commonConfig = appContext.get(CommonConfig);
+  const commonConfig = appContext.get(CommonConfig);
   // как бы вручную инжектим в инициализацию модуля нужную зависимость, донастраивая динамический модуль
-  const DynamicAppModule = await AppModule.forRoot();
+  const DynamicAppModule = await AppModule.forRoot(commonConfig);
 
   const app =
     await NestFactory.create<NestExpressApplication>(DynamicAppModule);
@@ -19,7 +20,7 @@ async function bootstrap() {
 
   useAppSettings(app);
 
-  await app.listen(4000);
+  await app.listen(commonConfig.port);
 }
 
 bootstrap();
--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,14 +1,105 @@
 import { DynamicModule, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
+import { seconds, ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UsersModule } from '../users/users.module';
+import { PostsModule } from '../posts/posts.module';
+import { BlogsModule } from '../blogs/blogs.module';
+import { CommentsModule } from '../comments/comments.module';
+import { AuthModule } from '../auth/auth.module';
+import { TestingModule } from '../testing/testing.module';
+import { CommonConfigModule } from '../../common/config/common-config.module';
+import { CommonConfig } from '../../common/config/common.config';
+import { LikesPostModule } from '../likes-post/likes-post.module';
+import { SecurityDevicesModule } from '../security-devices/security-devices.module';
+import { LikesCommentModule } from '../likes-comment/likes-comment.module';
+import { QuizQuestionsModule } from '../quiz-game/quiz-questions/quiz-questions.module';
+import { PairsQuizQuestionModule } from '../quiz-game/pairs-quiz-question/pairs-quiz-question.module';
+import { PairsQuizAnswerModule } from '../quiz-game/pairs-quiz-answer/pairs-quiz-answer.module';
+import { PairsQuizModule } from '../quiz-game/pairs-quiz/pairs-quiz.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    CqrsModule.forRoot(),
+    CommonConfigModule,
+    MongooseModule.forRootAsync({
+      imports: [CommonConfigModule],
+      useFactory: (config: CommonConfig) => ({
+        uri: config.dbHost,
+        auth: {
+          username: config.dbUser,
+          password: config.dbPass,
+        },
+        dbName: config.dbName,
+        retryAttempts: 5,
+        retryDelay: 3000,
+      }),
+      inject: [CommonConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [CommonConfigModule],
+      useFactory: (config: CommonConfig) => {
+        const ormConfig =
+          config.nodeEnv === 'production'
+            ? {
+                url: config.pgUrl,
+              }
+            : {
+                host: config.pgHost,
+                port: config.pgPort,
+                username: config.pgUser,
+                password: config.pgPassword,
+                database: config.pgDb,
+              };
+
+        return {
+          type: 'postgres',
+          synchronize: true,
+          autoLoadEntities: true,
+          ...ormConfig,
+        };
+      },
+      inject: [CommonConfig],
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [CommonConfigModule],
+      useFactory: (config: CommonConfig) => [
+        {
+          ttl: seconds(config.ttlRateLimit),
+          limit: config.numberRateLimit,
+        },
+      ],
+      inject: [CommonConfig],
+    }),
+    UsersModule,
+    LikesPostModule,
+    PostsModule,
+    BlogsModule,
+    CommentsModule,
+    AuthModule,
+    SecurityDevicesModule,
+    LikesCommentModule,
+    PairsQuizModule,
+    QuizQuestionsModule,
+    PairsQuizQuestionModule,
+    PairsQuizAnswerModule,
+  ],
 })
 export class AppModule {
-  static async forRoot(): Promise<DynamicModule> {
+  static async forRoot(commonConfig: CommonConfig): Promise<DynamicModule> {
     // Такой мудрёный способ мы используем, чтобы добавить к основным модулям необязательный модуль.
     // Чтобы не обращаться в декораторе(@Module) к переменной окружения через process.env, потому что
     // запуск декораторов происходит на этапе склейки всех модулей до старта жизненного цикла самого NestJS
     const additionalModules: any[] = [];
+    if (commonConfig.includeTestingModule) {
+      additionalModules.push(TestingModule);
+    }
 
     return {
       module: AppModule,
