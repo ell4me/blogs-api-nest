324622ec6dbf9c8e556613737d951a55dd8a98e4
Danil Sennikov <d.sennikov@banki.ru>
2025-04-01 21:53:23 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,14 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './modules/app/app.module';
 import { useAppSettings } from './common/helpers/useAppSettings';
-import { CommonConfig } from './common/config/common.config';
 
 async function bootstrap() {
   // из-за того, что нам нужно донастроить динамический AppModule, мы не можем сразу создавать приложение,
   // а создаём сначала контекст
   const appContext = await NestFactory.createApplicationContext(AppModule);
-  const commonConfig = appContext.get(CommonConfig);
+  // const commonConfig = appContext.get(CommonConfig);
   // как бы вручную инжектим в инициализацию модуля нужную зависимость, донастраивая динамический модуль
-  const DynamicAppModule = await AppModule.forRoot(commonConfig);
+  const DynamicAppModule = await AppModule.forRoot();
 
   const app =
     await NestFactory.create<NestExpressApplication>(DynamicAppModule);
@@ -20,7 +19,7 @@ async function bootstrap() {
 
   useAppSettings(app);
 
-  await app.listen(commonConfig.port);
+  await app.listen(4000);
 }
 
 bootstrap();
