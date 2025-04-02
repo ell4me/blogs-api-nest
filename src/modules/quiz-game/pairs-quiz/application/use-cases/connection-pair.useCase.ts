d7415fa67679a693457a447d4add90f94e83e199
Danil Sennikov <d.sennikov@banki.ru>
2025-04-02 21:04:28 +0300

    fix afters tests

--- src/modules/quiz-game/pairs-quiz/application/use-cases/connection-pair.useCase.ts
+++ src/modules/quiz-game/pairs-quiz/application/use-cases/connection-pair.useCase.ts
@@ -26,11 +26,13 @@ export class ConnectionPairUseCase
   async execute({
     userId,
   }: ConnectionPairCommand): Promise<TExecuteConnectionPair> {
+    console.error('Forbidden:', { userId });
     const currentActivePair =
       await this.pairsQuizRepository.getActiveOrPendingPair(userId);
+    console.error('403 Forbidden:', { userId, currentActivePair });
 
     if (currentActivePair) {
-      console.error('🚨 403 Forbidden:', { userId, currentActivePair });
+      console.error('🚨', { userId, currentActivePair });
       throw ForbiddenDomainException.create();
     }
 
